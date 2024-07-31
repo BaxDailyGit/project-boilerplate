4b4d940a09a34de387deac1d275362f46fd509d6
BaekSeungJin_백승진 <tmdwls8387@ajou.ac.kr>
2024-07-31 15:05:33 +0900

    feat: JWTUtil 주입

--- src/main/java/boilerplate/projectboilerplate/config/SecurityConfig.java
+++ src/main/java/boilerplate/projectboilerplate/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package boilerplate.projectboilerplate.config;
 
+import boilerplate.projectboilerplate.jwt.JWTUtil;
 import boilerplate.projectboilerplate.jwt.LoginFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,9 +20,13 @@ public class SecurityConfig {
     //AuthenticationManager가 인자로 받을 AuthenticationConfiguraion 객체 생성자 주입
     private final AuthenticationConfiguration authenticationConfiguration;
 
-    public SecurityConfig(AuthenticationConfiguration authenticationConfiguration) {
+    //JWTUtil 주입
+    private final JWTUtil jwtUtil;
+
+    public SecurityConfig(AuthenticationConfiguration authenticationConfiguration, JWTUtil jwtUtil) {
 
         this.authenticationConfiguration = authenticationConfiguration;
+        this.jwtUtil = jwtUtil;
     }
 
     //AuthenticationManager Bean 등록
@@ -60,8 +65,9 @@ public class SecurityConfig {
                         .anyRequest().authenticated());
 
         //필터 추가 LoginFilter()는 인자를 받음 (AuthenticationManager() 메소드에 authenticationConfiguration 객체를 넣어야 함) 따라서 등록 필요
+        //AuthenticationManager()와 JWTUtil 인수 전달
         http
-                .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration)), UsernamePasswordAuthenticationFilter.class);
+                .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration), jwtUtil), UsernamePasswordAuthenticationFilter.class);
 
         //세션 설정
         http
