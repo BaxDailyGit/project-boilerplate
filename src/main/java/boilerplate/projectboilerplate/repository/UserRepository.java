8116ad528ea6dd25691b3877476fb45f503a314c
BaekSeungJin_백승진 <tmdwls8387@ajou.ac.kr>
2024-07-31 14:30:43 +0900

    feat: DB연결 및 User Entity 작성

---
+++ src/main/java/boilerplate/projectboilerplate/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package boilerplate.projectboilerplate.repository;
+
+import boilerplate.projectboilerplate.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+
+}
