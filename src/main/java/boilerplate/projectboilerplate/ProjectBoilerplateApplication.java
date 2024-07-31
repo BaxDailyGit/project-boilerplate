cc15384eff319fd458e1486484680b2793e36d0e
BaekSeungJin_백승진 <tmdwls8387@ajou.ac.kr>
2024-07-31 13:46:21 +0900

    init

---
+++ src/main/java/boilerplate/projectboilerplate/ProjectBoilerplateApplication.java
@@ -0,0 +1,13 @@
+package boilerplate.projectboilerplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectBoilerplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectBoilerplateApplication.class, args);
+    }
+
+}
